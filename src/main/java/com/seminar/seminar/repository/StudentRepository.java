9f9b238c5e29c8ca02271bf3b475c3490fa6be76
billmazio <bmaziotis@windowslive.com>
2023-02-07 11:15:13 +0200

    StudentRepository.java

---
+++ src/main/java/com/seminar/seminar/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.seminar.seminar.repository;
+
+import com.seminar.seminar.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
