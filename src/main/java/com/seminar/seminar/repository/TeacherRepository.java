14060abda622a48594e2e6031386ab93f5af3171
billmazio <bmaziotis@windowslive.com>
2023-02-07 11:14:45 +0200

    TeacherRepository.java

---
+++ src/main/java/com/seminar/seminar/repository/TeacherRepository.java
@@ -0,0 +1,11 @@
+
+package com.seminar.seminar.repository;
+
+import com.seminar.seminar.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
+
